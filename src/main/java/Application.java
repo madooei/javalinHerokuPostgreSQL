38982e89b227ba5fb15ed8a12fa3ba5202ee3b62
Ali Madooei <madooei@jhu.edu>
2020-02-29 23:16:18 -0500

    Initial Commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,17 @@
+import io.javalin.Javalin;
+
+public class Application {
+  public static void main(String[] args) {
+    final int PORT = getHerokuAssignedPort();
+    Javalin app = Javalin.create().start(PORT);
+    app.get("/", ctx -> ctx.result("Hello Heroku!"));
+  }
+
+  private static int getHerokuAssignedPort() {
+    String herokuPort = System.getenv("PORT");
+    if (herokuPort != null) {
+      return Integer.parseInt(herokuPort);
+    }
+    return 7000;
+  }
+}
