d45b27b73d42e6c705bbc55214ea58619518ee66
Ali Madooei <madooei@jhu.edu>
2020-03-01 12:14:19 -0500

    Write to a database

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,18 @@
 import io.javalin.Javalin;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Application {
   public static void main(String[] args) {
     final int PORT = getHerokuAssignedPort();
     Javalin app = Javalin.create().start(PORT);
     app.get("/", ctx -> ctx.result("Hello Heroku!"));
+    workWithDatabase();
   }
 
   private static int getHerokuAssignedPort() {
@@ -13,5 +21,42 @@ public class Application {
       return Integer.parseInt(herokuPort);
     }
     return 7000;
+  }
+
+  private static void workWithDatabase(){
+    try (Connection conn = getConnection()) {
+
+      String sql = "CREATE TABLE IF NOT EXISTS Courses(" +
+          "name VARCHAR(30) NOT NULL," +
+          "url VARCHAR(100)" +
+          ");";
+      Statement st = conn.createStatement();
+      st.execute(sql);
+
+      sql = "INSERT INTO Courses(name, url) VALUES ('oose', 'jhu-oose.com');";
+      st.execute(sql);
+
+    } catch (URISyntaxException e) {
+      e.printStackTrace();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private static Connection getConnection() throws URISyntaxException, SQLException {
+    String databaseUrl = System.getenv("DATABASE_URL");
+    if (databaseUrl == null) {
+      // Not on Heroku, so use SQLite
+      return DriverManager.getConnection("jdbc:sqlite:./Store.db");
+      // This is a very bad practice: to run one DB locally and another on server
+    }
+
+    URI dbUri = new URI(databaseUrl);
+
+    String username = dbUri.getUserInfo().split(":")[0];
+    String password = dbUri.getUserInfo().split(":")[1];
+    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+
+    return DriverManager.getConnection(dbUrl, username, password);
   }
 }
